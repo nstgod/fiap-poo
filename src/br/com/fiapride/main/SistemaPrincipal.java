7f5f45f6908f7523eea3031ba26966496a3fc6fe
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 10:16:50 -0300

    Aula - Métodos e Comportamentos

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -5,28 +5,31 @@ import br.com.fiapride.model.Garrafa;
 
 public class SistemaPrincipal {
     public static void main(String[] args) {
-        // INSTANCIAÇÃO
-        // O comando 'new' aloca memória para um novo objeto.
-        // Criando o primeiro passageiro (Objeto 1)
-        Passageiro passageiro1 = new Passageiro();
-        passageiro1.nome = "Ana Silva";
-        passageiro1.saldo = 50.0;
 
-        // Criando o segundo passageiro (Objeto 2)
-        Passageiro passageiro2 = new Passageiro();
-        passageiro2.nome = "Carlos Souza";
-        passageiro2.saldo = 12.50;
+        Passageiro passageiro1 = new Passageiro("Ana silva", "222");
+        System.out.println("Recarga passageiro 1");
+        passageiro1.adicionarSaldo(50.0);
 
-        // Exibindo os dados no Console
+        Passageiro passageiro2 = new Passageiro("Nelson Troccoli", "123");
+        System.out.println("Recarga passageiro 2");
+        passageiro2.adicionarSaldo(15.0);
+
+
         System.out.println("--- Sistema FiapRide ---");
-        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo);
+        System.out.println("Passageiro: " + passageiro1.nome + " | Saldo: R$" + passageiro1.saldo + " | CPF: " + passageiro1.cpf);
 
-        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo);
+        System.out.println("Passageiro: " + passageiro2.nome + " | Saldo: R$" + passageiro2.saldo + " | CPF: " + passageiro2.cpf);
+
+        System.out.println("Pagando passagem do passageiro 1");
+        passageiro1.pagarViagem(20);
+        System.out.println("Pagando passagem do passageiro 2");
+        passageiro2.pagarViagem(20);
+
 
 
         // Teste mental: Se eu mudar o nome do passageiro1, o passageiro2 muda?
 
-        Garrafa minhaGarrafa = new Garrafa();
+        /*Garrafa minhaGarrafa = new Garrafa();
         minhaGarrafa.Cor = "Rosa";
         minhaGarrafa.Material = "Plastico";
         minhaGarrafa.capacidadeEmMl = 100;
@@ -37,6 +40,6 @@ public class SistemaPrincipal {
         guilhermeGarrafa.capacidadeEmMl = 50;
 
         System.out.println("Minha garrafa é: " + minhaGarrafa.Cor);
-        System.out.println("A garrafa do guilherme é: " + guilhermeGarrafa.Cor);
+        System.out.println("A garrafa do guilherme é: " + guilhermeGarrafa.Cor);*/
         }
 }
--- src/br/com/fiapride/model/Passageiro.java
+++ src/br/com/fiapride/model/Passageiro.java
@@ -3,6 +3,34 @@ package br.com.fiapride.model;
 public class Passageiro {
 
     public String nome;
+    public String cpf;
     public double saldo;
+
+    public Passageiro(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.saldo = 0.0;
+    }
+
+    public void adicionarSaldo(double valor) {
+        if (valor <= 0) {
+            System.out.println("O valor da recarga deve ser maior que zero");
+            return;
+        }
+        this.saldo += valor;
+        System.out.println("Recarga realizada. Novo saldo: " + this.saldo);
+    }
+
+    public void pagarViagem(double custo) {
+        if (custo <= 0) {
+            System.out.println("ERRO: O custo da viagem é inválido");
+        }
+        if (saldo < custo) {
+            System.out.println("ERRO: Saldo insuficiente para realizar a viagem");
+            return;
+        }
+        this.saldo -= custo;
+        System.out.println("Viagem paga. Saldo restante: " + this.saldo);
+    }
 
 }
